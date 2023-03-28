8389a1ab32b9f1d1446c1c8b79b45414f59d6122
matrixcsr <82505131+matrixcsr@users.noreply.github.com>
2023-03-28 14:31:56 -0400

    Delete providers.tf

--- providers.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  subscription_id = "9464d5d1-a5e9-4648-a14c-78f8cfc042ac"
-  tenant_id       = "5c8120ad-8039-4d40-91d0-c86c6d382b1b"
-  client_id       = "fd0bd62b-0048-4688-84d7-5bf651717c32"
-  client_secret   = "Tme8Q~l~6G~FKKxCOl2mkXKoBXN2vW_zBn7Ucdrc"
-}
